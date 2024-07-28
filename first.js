57787d325845567314233f7fe5a7343f6de81602
huyenn0 <huyen.ntn.clv@gmail.com>
2024-07-28 20:34:11 +0700

    initial CLV-01

---
+++ first.js
@@ -0,0 +1,30 @@
+var expected = function(val){
+    return{
+        tobe: function(expected){
+            if(val === expected){
+                return true
+            }else{
+                throw new Error("Not equal")
+            }
+        },
+        notToBe: function(expected){
+            if(val!=expected){
+                return true
+            }else{
+                throw new Error("Equal")
+            }
+        }
+    }
+}
+try{
+    console.log(expected(5).tobe(5))
+
+}catch(e){
+    console.log(e.Error)
+}
+try{
+    console.log(expected(5).tobe(null))
+
+}catch(e){
+    console.log(e.message)
+}
